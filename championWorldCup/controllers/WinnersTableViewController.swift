a2d8a1e13bd8e3d4db487ba1b5c8a2d0fe66a274
thaylon92 <thaylonmiller@gmail.com>
2022-01-14 16:05:30 -0300

    Preenchendo celulas UITableViewCell

--- championWorldCup/controllers/WinnersTableViewController.swift
+++ championWorldCup/controllers/WinnersTableViewController.swift
@@ -8,38 +8,49 @@
 import UIKit
 
 class WinnersTableViewController: UITableViewController {
-
+   
+    var worldCups: [WorldCup] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        loadWorldCups()
+    }
+    
+    func loadWorldCups() {
+        let fileURL = Bundle.main.url(forResource: "winners.json", withExtension: nil)!
+        let jsonData = try! Data(contentsOf: fileURL)
+        do {
+            worldCups = try JSONDecoder().decode([WorldCup].self, from: jsonData)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
     }
 
     // MARK: - Table view data source
-
+    /*
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0
     }
-
+     */
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return worldCups.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let worldCup = worldCups[indexPath.row]
+        cell.textLabel?.text = "copa \(worldCup.year) - \(worldCup.country)"
+        cell.detailTextLabel?.text = "\(worldCup.winner) vs \(worldCup.vice)"
+        cell.imageView?.image = UIImage(named: "\(worldCup.winner).png")
         return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.
